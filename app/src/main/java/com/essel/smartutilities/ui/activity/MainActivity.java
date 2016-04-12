e9dcd4bf7c9508710b48cf7bc55201217f2a6749
akshaykumarM <akshay.kumar@tungstenbigdata.com>
2016-04-12 12:45:59 +0530

    initial commit

--- app/src/main/java/com/essel/smartutilities/ui/activity/MainActivity.java
+++ app/src/main/java/com/essel/smartutilities/ui/activity/MainActivity.java
@@ -1,13 +1,15 @@
-package com.essel.smartutilities;
+package com.essel.smartutilities.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.essel.smartutilities.R;
 
 public class MainActivity extends AppCompatActivity {
 
